6f99d2d6c677babec9d031560f1269a27b7c922c
eduardoyanoliveira <63554709+eduardoyanoliveira@users.noreply.github.com>
2022-09-29 19:38:26 -0300

    fixing admin screen errors

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from users.models import CustomUser
 
-# Register your models here.
-
-admin.site.register(CustomUser)
+class UserAdminConfig(UserAdmin):
+    model = CustomUser
+    list_filter = ('email', 'username' , 'is_active')
+    ordering = ('-created_at',)
+    list_display = ('id', 'email', 'username',
+                    'is_active', 'is_admin', 'created_at',)
+    
+    filter_horizontal = ()
+    
+    fieldsets = (
+        (None, {'fields': ('email', 'username',)}),
+        ('Permissions', {'fields': ('is_admin', 'is_active')}),
+    )
+    
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username',  'password1', 'password2', 'is_active', 'is_admin',)}
+        ),
+    )
+    
+admin.site.register(CustomUser, UserAdminConfig)
