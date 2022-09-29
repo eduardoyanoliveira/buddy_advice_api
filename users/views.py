cace708fdcaba2bc51bc1c73a1e242facae0f43c
eduardoyanoliveira <63554709+eduardoyanoliveira@users.noreply.github.com>
2022-09-29 18:57:48 -0300

    correcting base url on users

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from rest_framework.viewsets import ModelViewSet
 from users.models import CustomUser
 from users.serializers import CustomUserSerializer
 
-class CustomUserModelViewSet(ModelViewSet):
+class CustomUserViewSet(ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
      
