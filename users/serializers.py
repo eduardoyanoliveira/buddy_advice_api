0c1dc0ea5e2e3163920e9492d3deec645181bd1f
eduardoyanoliveira <63554709+eduardoyanoliveira@users.noreply.github.com>
2022-09-30 14:34:51 -0300

    fixing password error
    Api was not saving the password on database

--- users/serializers.py
+++ users/serializers.py
@@ -2,7 +2,24 @@ from users.models import CustomUser
 from rest_framework import serializers
 
 class CustomUserSerializer(serializers.ModelSerializer):
+    
+    email = serializers.EmailField(required=True)
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(min_length=6, write_only=True)
+
     class Meta:
         model = CustomUser
-        fields = ('id', 'username', 'email', 'is_active', 'is_admin', 'created_at')
+        fields = '__all__'
+        extra_kwargs = {'password': {'write_only': True}}
+      
+        
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        # as long as the fields are the same, we can just use this
+        user = self.Meta.model(**validated_data)
+        if password is not None:
+            user.set_password(password)
+        user.save()
+        return user 
+   
      
