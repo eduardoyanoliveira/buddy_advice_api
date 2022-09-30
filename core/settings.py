de61f86f2774e75747b9199c134120afe6cd0c7d
eduardoyanoliveira <63554709+eduardoyanoliveira@users.noreply.github.com>
2022-09-30 13:41:21 -0300

    bug: fixing create user error 'Token not found'

--- core/settings.py
+++ core/settings.py
@@ -143,6 +143,7 @@ REST_FRAMEWORK = {
     )
 }
 
+
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=5),
--- users/views.py
+++ users/views.py
@@ -3,15 +3,19 @@ from users.models import CustomUser
 from users.serializers import CustomUserSerializer
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
+
+
 class CustomUserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
     
+
     def get_permissions(self):
         try:
-            if self.action == 'create':
+            if self.action in ['create']:
                 return [AllowAny()]
             
             return [IsAuthenticated()]
         except:
             return Response({'msg': 'Internal Server Error'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
