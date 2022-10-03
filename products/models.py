e2e5be12b83d902e6e31f0046f764bc57019319e
eduardoyanoliveira <63554709+eduardoyanoliveira@users.noreply.github.com>
2022-10-03 13:33:09 -0300

    add description field to product model

--- products/models.py
+++ products/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class ProductModel(models.Model):
     
     name = models.CharField(max_length=70, null=False, blank=False)
+    description = models.TextField(max_length=600, null=False, blank=False)
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
